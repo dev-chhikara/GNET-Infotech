4d28cb07c911e73a00669f6b489fcae8732e3d96
dev-chhikara <devchhikara05@gmail.com>
2024-12-05 19:38:45 +0530

    updated accounts v15

--- login/script.js
+++ login/script.js
@@ -5,6 +5,7 @@ import {
     browserLocalPersistence,
     RecaptchaVerifier,
     onAuthStateChanged,
+    updateProfile,
     signInWithPhoneNumber,
     PhoneAuthProvider,
     signInWithCredential,
